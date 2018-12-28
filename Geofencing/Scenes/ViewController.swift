cdf4b62abba2b242813c2b3bfccfb23419ed0499
SerhiiM <msm72@mail.ru>
2018-12-28 05:09:04 +0200

    Create project folders.

---
+++ Geofencing/Scenes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Geofencing
+//
+//  Created by msm72 on 12/28/18.
+//  Copyright © 2018 golos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
